0489d132d2354af0ca29e37f4e88c6ce660587fe
Titus <tituswormer@gmail.com>
2021-10-28 11:41:52 +0200

    Add improved docs
    Closes GH-63.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Merlijn Vos <merlijn@soverin.net>
    Reviewed-by: Remco Haszing <remcohaszing@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * @typedef {import('./lib/index.js').UrlConfig} UrlConfig
+ * @typedef {import('./lib/index.js').Options} Options
+ */
+
 import remarkValidateLinks from './lib/index.js'
 
 export default remarkValidateLinks
